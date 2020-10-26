4a2d713e2852cd7991782c84d24019d3101ab6a1
palaashGupta1 <65058970+palaashGupta1@users.noreply.github.com>
2020-10-26 18:47:11 +0530

    Add files via upload

---
+++ js/rank.js
@@ -0,0 +1,12 @@
+class Rank {
+constructor(){
+
+}
+display(){
+    var victory=createElement('h3');
+    victory.position(displayWidth/2-100,displayHeight/2);
+    victory.html("player Rank: "+player.rank);
+}
+
+
+}
